e8c0d63177e17382ead54b51d4f9114fa1dfbb56
huming <ming@strikingly.com>
2014-10-31 15:43:45 +0800

    Initial Commit

---
+++ matchismo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  matchismo
+//
+//  Created by HU Ming on 31/10/14.
+//  Copyright (c) 2014 HU Ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  matchismo
+//
+//  Created by HU Ming on 31/10/14.
+//  Copyright (c) 2014 HU Ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
